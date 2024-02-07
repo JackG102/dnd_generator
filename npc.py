4968cd91e989208548ef7703a9f46345f594a8fb
Jack Garratt <jack.garratt@icfnext.com>
2024-02-06 19:22:08 -0500

    Coded gender method for Npc class

--- npc.py
+++ npc.py
@@ -1,4 +1,6 @@
-class npc:
+import random
+
+class Npc:
   def __init__(self, first_name, last_name, gender, race, vocation):
     self.first_name = first_name
     self.last_name = last_name
@@ -6,4 +8,11 @@ class npc:
     self.race = race
     self.vocation = vocation
 
-  def gender:
+  def assignGender(self):
+    gender_list = ['male', 'female', 'non-binary']
+    self.gender = random.choice(gender_list)
+
+testNPC = Npc('Jack', 'Garratt', 'foo', 'human', 'developer')
+print(testNPC.gender)
+testNPC.assignGender()
+print(testNPC.gender)
