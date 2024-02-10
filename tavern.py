cacc94dc1e444cdbb584eb7e04fc96548cb5e344
Jack Garratt <jack.garratt@icfnext.com>
2024-02-10 15:10:48 -0500

    Refactored call to list files with file refactored locations

--- tavern.py
+++ tavern.py
@@ -2,22 +2,20 @@ import os
 import random
 import sys
 from pathlib import Path
-from npc import Npc
 
 class Tavern:
+  project_location = str(Path(__file__).parent)
+  lists_folder_location = project_location + '/lists/'
 
   def __init__(self):
     self.assign_name()
 
   def assign_name(self):
-    project_location = str(Path(__file__).parent.parent)
-    lists_folder_location = project_location + '/lists/'
-
-    adjective_list_file = open(os.path.join(os.path.dirname(lists_folder_location), 'adjectives.txt'))
+    adjective_list_file = open(os.path.join(os.path.dirname(self.lists_folder_location), 'adjectives.txt'))
     adjective_lines = adjective_list_file.read().splitlines() 
     adjective_name = random.choice(adjective_lines)
 
-    animal_list_file = open(os.path.join(os.path.dirname(lists_folder_location), 'animals.txt'))
+    animal_list_file = open(os.path.join(os.path.dirname(self.lists_folder_location), 'animals.txt'))
     animal_lines = animal_list_file.read().splitlines() 
     animal_name = random.choice(animal_lines)
 
